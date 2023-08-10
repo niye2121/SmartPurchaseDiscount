d15c41897be06f58b8847295654ed47241eb4e57
Queeni <queeni@Queenis-MacBook-Pro.local>
2023-08-10 05:47:59 +0300

    Module Structure

---
+++ smart_purchase_discount/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Smart Purchase Discount",
+
+    'summary': """
+       Smart Purchase Discount""",
+
+    'description': """
+       Smart Purchase Discount
+    """,
+
+    'author': "Nigist Mamo",
+
+    'category': 'Purchase',
+    'version': '0.1',
+    'depends': ['base'],
+
+    'data': [
+        'views/views.xml',
+    ],
+}

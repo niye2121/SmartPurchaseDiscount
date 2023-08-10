8a325ef2d4e190a9c44721c5663ed44c7e606e0e
Queeni <queeni@Queenis-MacBook-Pro.local>
2023-08-10 06:51:50 +0300

    add purchase dependency

--- smart_purchase_discount/models/res_config_settings.py
+++ smart_purchase_discount/models/res_config_settings.py
@@ -6,5 +6,7 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     activate_discount = fields.Boolean("Activate Discount")
-    discount_method = fields.Selection( [('fixed', 'Fiixed'), ('percentage', 'Percentage'),])
+    discount_method = fields.Selection( [('fixed', 'Fixed'), ('percentage', 'Percentage'),])
+    apply_on = fields.Selection( [('order_line', 'Order Line'), ('total', 'Total Value'),])
+    calculate = fields.Selection( [('including_tax', 'Including Tax'), ('exclude_tax', 'Excluding tax'),])
     fixed_discount_amount = fields.Float("Fixed Discount Amount")
